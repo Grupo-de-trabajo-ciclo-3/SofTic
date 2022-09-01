3d1f2c3a52586135b0a2a577e3c60eea8e3e679f
jomallaru <jomallaru@gmail.com>
2022-08-31 23:32:10 -0500

    Iwinser verifica por favor

--- src/main/java/com/softic/SofTA/entities/MovimientoDinero.java
+++ src/main/java/com/softic/SofTA/entities/MovimientoDinero.java
@@ -1,4 +1,38 @@
 package com.softic.SofTA.entities;
 
 public class MovimientoDinero {
+    String conceptoMovimiento;
+    int montoMovimiento;
+    String users;
+    int acumulador = 0;
+    public MovimientoDinero(String conceptoMovimiento, int montoMovimiento, String users) {
+        this.conceptoMovimiento = conceptoMovimiento;
+        this.montoMovimiento = montoMovimiento;
+        this.users = users;
+    }
+
+
+    public int getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(int montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public String getUsers() {
+        return users;
+    }
+
+    public void setUsers(String users) {
+        this.users = users;
+    }
+
+    public int getAcumulador() {
+        return acumulador;
+    }
+
+    public void setAcumulador(int acumulador) {
+        this.acumulador = acumulador;
+    }
 }
