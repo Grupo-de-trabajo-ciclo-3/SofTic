30c87c287a877fc8728cf95f3ff261a807a10b93
juan12109 <juan.991116@gmail.com>
2022-09-01 00:26:08 -0500

    agrego algunos atributos que son independientes en la clase empleado

--- src/main/java/com/softic/SofTA/entities/Empleado.java
+++ src/main/java/com/softic/SofTA/entities/Empleado.java
@@ -3,5 +3,20 @@ package com.softic.SofTA.entities;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "empleado")
 public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "correo")
+    private String correo;
+
+ 
+
 }
