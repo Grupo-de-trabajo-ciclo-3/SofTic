bc70313b334216caa073b76ea97e05d43a326e87
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-09-10 22:22:34 -0500

    creando controlador empleado

--- proyectoC3/src/main/java/com/example/demo/controladores/Controldinero.java
+++ proyectoC3/src/main/java/com/example/demo/controladores/Controldinero.java
@@ -1,4 +1,10 @@
-/*package controladores;
+package controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class Controldinero {
-}*/
+    @GetMapping("/verdinero")
+    public String ver(){
+        return "holamundo funciono en local para ver dinero";
+    }
+}
