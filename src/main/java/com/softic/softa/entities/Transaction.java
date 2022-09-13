c23fd822b53d898e8ddf03be1b3ef4c6004b49b8
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-09-12 20:12:42 -0500

    primero commit

--- src/main/java/com/softic/softa/entities/Transaction.java
+++ src/main/java/com/softic/softa/entities/Transaction.java
@@ -1,5 +1,26 @@
 package com.softic.softa.entities;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor //Al modelar las clases como entidades hay que tener un constructor vacío para evitar errores a futuro.
+@Getter
+@Setter
+@ToString
+@Table(name = "transaction")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_transaction", nullable = false)
+    private Long idTransaction;
+
+    //Atributos
+    @Column(name = "concept_movements")
+    private String conceptMovements;
+    @Column(name = "amount_movements")
+    private float amountMovements;
 
 }
