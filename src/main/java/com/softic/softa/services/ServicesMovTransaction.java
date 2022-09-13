13040c7a96d40a08d14303848d44d2700d091867
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-09-12 20:29:13 -0500

    creando servicio -

--- src/main/java/com/softic/softa/services/ServicesMovTransaction.java
+++ src/main/java/com/softic/softa/services/ServicesMovTransaction.java
@@ -1,5 +1,31 @@
 package com.softic.softa.services;
 
+import com.softic.softa.entities.Transaction;
+import com.softic.softa.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ServicesMovTransaction {
+    @Autowired //Anotación para especificarle a Spring Boot que aquí estoy haciendo una inyección de dependencias
+    TransactionRepository transactionRepository;
+
+    //Metodos
+    public void crearYActualizarTransaction(Transaction transaction){
+        transactionRepository.save(transaction);
+    }
+
+    public List<Transaction> verTransaction(){
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        transactions.addAll(transactionRepository.findAll());
+        return transactions;
+    }
+
+    public void eliminarTransaction(Long idEnterprise){
+        transactionRepository.deleteById(idEnterprise);
+    }
 
 }
